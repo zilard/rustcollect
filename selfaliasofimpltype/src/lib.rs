e78d7d1f4470b8e34ac2b2c4ccfa074dc64de88f
zilard <zilard.cherry@gmail.com>
2022-04-28 01:01:06 +0200

    add more associated functions

--- selfaliasofimpltype/src/lib.rs
+++ selfaliasofimpltype/src/lib.rs
@@ -25,10 +25,42 @@ pub struct Rectangle {
     pub height: u32,
 }
 
+
+// All functions defined within an impl block are called associated functions 
+// because they’re associated with the type named after the impl.
 impl Rectangle {
+
     pub fn area(&self) -> u32 {
         self.width * self.height
     }
+
+
+    pub fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+
+    pub fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+
+    pub fn print(&self) -> String {
+
+        format!("Rectangle width: {} and height: {}", self.width, self.height)
+
+    }
+
+
+
 }
 
 
--- selfaliasofimpltype/src/main.rs
+++ selfaliasofimpltype/src/main.rs
@@ -18,9 +18,38 @@ fn main() {
         height: 50,
     };
 
-   println!(
-       "The area of the rectangle is {} square pixels.",
-       rect1.area()
-   );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+
+    if rect1.width() {
+        println!(
+            "The rectangle has a nonzero width; it is {}",
+            rect1.width);
+    };
+
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+   
+    
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+
+    // the :: syntax is used for both associated functions 
+    // and namespaces created by modules.
+    let sq = Rectangle::square(3);
+
+    println!("The size of the newly created rectangle: {}", sq.print());
 
 }
