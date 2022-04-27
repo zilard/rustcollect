5f171d3e76ba0fb80a6fefb7d0be6d3865f4aabd
zilard <zilard.cherry@gmail.com>
2022-04-27 21:16:07 +0200

    function returning Trait - v1

---
+++ returntraits/src/main.rs
@@ -0,0 +1,91 @@
+
+use returntraits::{Summary, Display, Clone, Debug, Dm, NewsArticle, Tweet};
+
+/*
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+*/
+
+pub fn notify<T: Summary + Display>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+
+pub fn some_function<T, U>(t: &T, u: &U) -> i32
+    where T: Display + Clone,
+          U: Clone + Debug
+{
+    1
+}
+
+
+//You don't need the `return` keyword in Rust, 
+// if the last expression in a function body (or any other body) 
+// is not finished with a semicolon, then it is the return value
+
+pub fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
+
+
+
+
+fn main() {
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+
+
+
+
+
+    notify(&tweet);
+
+
+
+
+
+
+    let dm = Dm {};
+   
+    println!("Function returns {}", some_function(&article, &dm));
+
+
+
+    println!("This returns {}", returns_summarizable().summarize());
+
+
+}
+
+
