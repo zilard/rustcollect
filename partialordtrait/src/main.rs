06367a7d4cfb5a9b167f6e78766857d041b46805
zilard <zilard.cherry@gmail.com>
2022-04-27 22:44:27 +0200

    implement partialorder on vectors

---
+++ partialordtrait/src/main.rs
@@ -0,0 +1,26 @@
+// we need to specify PartialOrd in the trait bounds for T
+// so the largest function can work on slices of any type that we can compare.
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
