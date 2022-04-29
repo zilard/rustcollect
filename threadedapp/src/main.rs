2494610bcaadbbdf72ddf503108f4f60e759c531
zilard <zilard.cherry@gmail.com>
2022-04-29 17:59:01 +0200

    refactoring threaded app

--- threadedapp/src/main.rs
+++ threadedapp/src/main.rs
@@ -17,25 +17,17 @@ fn main() {
 
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-             simulated_expensive_calculation(intensity)
+    let expensive_result = simulated_expensive_calculation(intensity);
 
-        );
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result);
+        println!("Next, do {} situps!", expensive_result);
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!(
-                "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
-            );
-
+            println!("Today, run for {} minutes!", expensive_result);
         }
 
     }
