34554e08a14c904944477a78b794525f76d92773
zilard <zilard.cherry@gmail.com>
2022-04-29 18:06:40 +0200

    refactoring with closure

--- threadedapp/src/main.rs
+++ threadedapp/src/main.rs
@@ -1,12 +1,6 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
 fn main() {
     let simulated_user_specified_value = 26;
     let simulated_random_number = 10;
@@ -15,22 +9,27 @@ fn main() {
 
 }
 
-
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
 
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
 
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_result);
-        println!("Next, do {} situps!", expensive_result);
+        println!("Today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes!", expensive_result);
+            println!(
+                "Today, run for {} minutes!", 
+                expensive_closure(intensity)
+            );
         }
 
     }
 
 }
-
