cc59252c9460ad6733c4cd693297df0928c5aadf
zilard <zilard.cherry@gmail.com>
2022-04-30 11:51:37 +0200

    adding tests

--- checkpermutation/src/main.rs
+++ checkpermutation/src/main.rs
@@ -15,19 +15,50 @@ fn count_chars(s: &str) -> HashMap<char, i32> {
     characters
 }
 
+fn is_permutation(s1: &str, s2: &str) -> bool {
+    let characters_1 = count_chars(s1);
+    let characters_2 = count_chars(s2);
 
+
+    for key in characters_1.keys() {
+        if !characters_2.contains_key(&key) {
+            return false;
+        }
+        if characters_1.get(&key) != characters_2.get(&key) {
+            return false;
+        }
+    }
+
+    for key in characters_2.keys() {
+        if !characters_1.contains_key(&key) {
+            return false;
+        }
+    }
+
+    true
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_permutation() {
+        assert_eq!(
+            is_permutation(&String::from("cat"), &String::from("tac")),
+            true
+        );
+        assert_eq!(
+            is_permutation(&String::from("cat"), &String::from("dog")),
+            false
+        );
+    }
+
+}
 
 
 fn main() {
-   
- 
-    //is_permutation(&String::from("cat"), &String::from("dog"));
-
-    let string_hash = count_chars(&String::from("ccat"));
-
-    for (key, value) in &string_hash {
-        println!("{}: {}", key, value);
-    }
-
+    is_permutation(&String::from("cat"), &String::from("dog"));
 }
 
