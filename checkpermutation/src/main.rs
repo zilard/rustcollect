37e0f63d9c15c0e0bfb40a27e262a070155290e0
zilard <zilard.cherry@gmail.com>
2022-04-30 11:02:38 +0200

    initial HashMap implementation

--- checkpermutation/src/main.rs
+++ checkpermutation/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+// Given two strings, write a method to decide if one 
+// is a permutation of the other.
+
+use std::collections::HashMap;
+
+fn count_chars(s: &str) -> HashMap<char, i32> {
+    let mut characters: HashMap<char, i32> = HashMap::new();
+
+    for c in s.chars() {
+
+        // insert a key only if it doesn't already exist
+        let x = characters.entry(c).or_insert(0);
+        *x += 1;
+    }
+    characters
 }
+
+
+
+
+fn main() {
+   
+ 
+    //is_permutation(&String::from("cat"), &String::from("dog"));
+
+    let string_hash = count_chars(&String::from("ccat"));
+
+    for (key, value) in &string_hash {
+        println!("{}: {}", key, value);
+    }
+
+}
+
