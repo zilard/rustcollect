9d2a0e20ae8f2675a09ee4418cb174be0dcd1b4f
zilard <zilard.cherry@gmail.com>
2022-05-20 00:35:49 +0200

    adding further test to single linked list implementation

--- singlelinkedlist/src/main.rs
+++ singlelinkedlist/src/main.rs
@@ -131,6 +131,33 @@ mod tests {
 
     }
 
+
+    #[test]
+    fn test_series_of_pops_and_inserts() {
+
+        let mut list: SinglyLinkedList<usize> = SinglyLinkedList::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(3);
+        list.push(42);
+        assert_eq!(list.pop(), Some(42));
+        assert_eq!(list.len(), 1);
+
+        list.push(93);
+        assert_eq!(list.len(), 2);
+        assert_eq!(list.pop(), Some(93));
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), None);
+        assert_eq!(list.len(), 0);
+
+        list.push(20);
+        assert_eq!(list.pop(), Some(20));
+        assert_eq!(list.pop(), None);
+
+    }
+
+
 }
 
 
