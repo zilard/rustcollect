c679a1585443e12b642c4c68c9a98e8fea4b471c
zilard <zilard.cherry@gmail.com>
2023-06-03 14:28:35 +0200

    tokio async chat server

---
+++ lifetime2/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let string1 = String::from("abcd");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    println!("The longest string is {}", result);
+}
+
+fn longest<'a>(x: &str, y: &str) -> String {
+    let result = String::from("really long string");
+    result
+}
