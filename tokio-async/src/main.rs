e01b2c21da0f4d82856b7c935fa690722cfdde93
zilard <zilard.cherry@gmail.com>
2023-06-05 00:29:32 +0200

    basic server using async and await

---
+++ tokio-async/src/main.rs
@@ -0,0 +1,17 @@
+use tokio::time;
+use log::Level;
+
+async fn run() {
+    log::info!("Sleeping");
+    time::sleep(time::Duration::from_secs(1)).await;
+    log::info!("Awake!");
+}
+
+fn main() {
+    simple_logger::init_with_level(Level::Info).unwrap();
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let future = run();
+
+    rt.block_on(future);
+}
