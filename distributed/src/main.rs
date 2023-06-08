72ee0ee204032d21a9f5f14e030a305d8478a9d7
zilard <zilard.cherry@gmail.com>
2023-06-08 17:21:56 +0200

    defining initial data sturcture

---
+++ distributed/src/main.rs
@@ -0,0 +1,33 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Message {
+    src: String,
+    #[serde(rename = "dest")]
+    dest: String,
+    body: Body,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Body {
+    #[serde(rename = "msg_id")]
+    id: Option<usize>,
+    in_reply_to: Option<usize>,
+    #[serde(flatten)]
+    payload: Payload,
+}
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Echo {
+        echo: String 
+    },
+}
+
+
+fn main() {
+    println!("Hello!");
+}
