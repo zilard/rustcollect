bac7a81c41f7e8060ee82578e13b5778b2edf499
zilard <zilard.cherry@gmail.com>
2023-06-08 22:46:42 +0200

    implementing message sending receiveing state machine

--- distributed/src/main.rs
+++ distributed/src/main.rs
@@ -1,10 +1,13 @@
+use std::io::StdoutLock;
+
 use serde::{Deserialize, Serialize};
+use anyhow::Context;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Message {
     src: String,
     #[serde(rename = "dest")]
-    dest: String,
+    dst: String,
     body: Body,
 }
 
@@ -22,12 +25,58 @@ struct Body {
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 enum Payload {
-    Echo {
-        echo: String 
-    },
+    Echo { echo: String },
+    EchoOk { echo: String },
 }
 
+struct EchoNode {
+    id: usize,
+}
 
-fn main() {
-    println!("Hello!");
+//interface
+impl EchoNode {
+    pub fn step(
+        &mut self, 
+        input: Message, 
+        output: &mut serde_json::Serializer<StdoutLock>
+    ) -> anyhow::Result<()> {
+        match input.body.payload {
+            Payload::Echo { echo } => {
+                let reply = Message {
+                    src: input.dst,
+                    dst: input.src,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::EchoOk { echo },
+                    },
+                };
+                reply.serialize(output).context("serialize response to echo")?;
+                self.id += 1;
+                
+            }
+            Payload::EchoOk { .. } => {}            
+        }
+        Ok(())
+    }
+
+}
+
+fn main() -> anyhow::Result<()> {
+    let stdin = std::io::stdin().lock();
+    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
+
+    let stdout = std::io::stdout().lock();
+    let mut output = serde_json::Serializer::new(stdout);
+
+    let mut state = EchoNode { id: 0 };
+
+    for input in inputs {
+        let input = input.context("Maelstrom input from STDIN could not be deserialized")?;
+        state
+            .step(input, &mut output)
+            .context("Node step function failed")?;
+    }
+
+    Ok(())
 }
